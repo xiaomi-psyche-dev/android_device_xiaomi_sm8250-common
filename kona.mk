cd20ec06295dee3752148ce6a4a1377bca99d4e6
Davide Garberi <dade.garberi@gmail.com>
2024-04-01 15:14:11 +0200

    sm8250-common: Build `libhidltransport` and `libhwbinder`
    05-05 15:53:59.961 12354 12354 E MI_Algo_JNI: Open /system/lib64/libmicampostproc_client.so Error :dlopen failed: library "libhidltransport.so" not found: needed by /system/lib64/libmicampostproc_client.so in namespace clns-shared-4
    
    Change-Id: Id87af3af701000734aaa89f1ec98a80dd70a266b
    Signed-off-by: RobertGarciaa <chae0218@naver.com>
    Signed-off-by: mtkpapa <ebanashka10028@gmail.com>

--- kona.mk
+++ kona.mk
@@ -215,6 +215,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti_recovery
 endif
 
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhwbinder
+
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
