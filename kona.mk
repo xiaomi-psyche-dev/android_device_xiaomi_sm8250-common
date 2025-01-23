0487bd1294be1682bf1cb1a9cf11a0014b9dcd29
Verevka <verevka86@gmail.com>
2025-01-23 10:46:41 +0300

    sm8250-common: Adding needed MiuiCamera libraries
    Signed-off-by: mtkpapa <ebanashka10028@gmail.com>

--- kona.mk
+++ kona.mk
@@ -145,6 +145,12 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
+PRODUCT_PACKAGES += \
+    libutils.vendor \
+    libstdc++_vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
