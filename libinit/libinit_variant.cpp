dc1bc7f77291ba0371f6c1cf49c1aa24627baf05
mtkpapa <ebanashka10028@gmail.com>
2025-12-09 17:50:24 +0300

    sm8250-common: libinit: Add support of hiding various props required for MIUI apps
    - Hide hardware sku
    - Hide .mod_device
    - Hide .cert
    - Hide flavor
    
    Signed-off-by: mtkpapa <ebanashka10028@gmail.com>

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -40,10 +40,16 @@ void set_variant_props(const variant_info& variant) {
     auto marketname = !variant.marketname.empty() ? variant.marketname : variant.model;
 
     set_ro_build_prop("brand", variant.brand, true);
+    set_ro_build_prop("cert", variant.cert, true);
     set_ro_build_prop("device", variant.device, true);
     set_ro_build_prop("marketname", marketname, true);
     set_ro_build_prop("model", variant.model, true);
+    set_ro_build_prop("mod_device", variant.mod_device, true);
+    set_ro_build_prop("name", variant.name, true);
+    
     property_override("vendor.usb.product_string", marketname, true);
+    property_override("ro.boot.hardware.sku", variant.hwsku, true);
+    property_override("ro.build.flavor", variant.flavor, true);
 
     if (access("/system/bin/recovery", F_OK) != 0) {
         property_override("bluetooth.device.default_name", marketname, true);
