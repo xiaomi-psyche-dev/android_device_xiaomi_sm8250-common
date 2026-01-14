b349b153b7cd6ceea57ccbada744b84862786139
mtkpapa <ebanashka10028@gmail.com>
2026-01-14 12:35:40 +0300

    sm8250-common: Add dalvik-heap device configs for 8/12/16 GiB devices
    ref: https://github.com/mtkpapa/device_xiaomi_sm8250-common/commit/edae7c39efe830757b58007c908cd5e5e5024479
    Signed-off-by: mtkpapa <ebanashka10028@gmail.com>

--- libinit/libinit_dalvik_heap.cpp
+++ libinit/libinit_dalvik_heap.cpp
@@ -29,6 +29,33 @@ struct dalvik_heap_info {
     std::string heaptargetutilization;
 };
 
+static const dalvik_heap_info dalvik_heap_info_16384 = {
+        .heapstartsize = "32m",
+        .heapgrowthlimit = "448m",
+        .heapsize = "640m",
+        .heapminfree = "16m",
+        .heapmaxfree = "64m",
+        .heaptargetutilization = "0.4",
+};
+
+static const dalvik_heap_info dalvik_heap_info_12288 = {
+        .heapstartsize = "24m",
+        .heapgrowthlimit = "384m",
+        .heapsize = "512m",
+        .heapminfree = "8m",
+        .heapmaxfree = "56m",
+        .heaptargetutilization = "0.42",
+};
+
+static const dalvik_heap_info dalvik_heap_info_8192 = {
+        .heapstartsize = "24m",
+        .heapgrowthlimit = "256m",
+        .heapsize = "512m",
+        .heapminfree = "8m",
+        .heapmaxfree = "48m",
+        .heaptargetutilization = "0.46",
+};
+
 static const dalvik_heap_info dalvik_heap_info_6144 = {
         .heapstartsize = "16m",
         .heapgrowthlimit = "256m",
@@ -38,37 +65,20 @@ static const dalvik_heap_info dalvik_heap_info_6144 = {
         .heaptargetutilization = "0.5",
 };
 
-static const dalvik_heap_info dalvik_heap_info_4096 = {
-        .heapstartsize = "8m",
-        .heapgrowthlimit = "256m",
-        .heapsize = "512m",
-        .heapminfree = "8m",
-        .heapmaxfree = "16m",
-        .heaptargetutilization = "0.6",
-};
-
-static const dalvik_heap_info dalvik_heap_info_2048 = {
-        .heapstartsize = "8m",
-        .heapgrowthlimit = "192m",
-        .heapsize = "512m",
-        .heapminfree = "512k",
-        .heapmaxfree = "8m",
-        .heaptargetutilization = "0.75",
-};
-
 void set_dalvik_heap() {
     struct sysinfo sys;
     const dalvik_heap_info* dhi;
 
     sysinfo(&sys);
 
-    if (sys.totalram > GB(5)) {
+    if (sys.totalram > GB(15))
+        dhi = &dalvik_heap_info_16384;
+    else if (sys.totalram > GB(11))
+        dhi = &dalvik_heap_info_12288;
+    else if (sys.totalram > GB(7))
+        dhi = &dalvik_heap_info_8192;
+    else
         dhi = &dalvik_heap_info_6144;
-    } else if (sys.totalram > GB(3)) {
-        dhi = &dalvik_heap_info_4096;
-    } else {
-        dhi = &dalvik_heap_info_2048;
-    }
 
     property_override(kHeapStartSizeProp, dhi->heapstartsize);
     property_override(kHeapGrowthLimitProp, dhi->heapgrowthlimit);
