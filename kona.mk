1368491c97af63a75f5ccfdb7e974854d69b33fd
johnmart19 <johnivan19999@gmail.com>
2026-07-23 05:30:51 +0300

    sm8250-common: A17: Build libperfmgr for Lineage Power Service
    I init    : starting service 'vendor.power-hal-aidl'...
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.power-service.lineage-libperfmgr": library "libperfmgr.so" not found: needed by main executable
    
    Signed-off-by: mtkpapa <ebanashka10028@gmail.com>

--- kona.mk
+++ kona.mk
@@ -319,7 +319,8 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
-    libqti-perfd-client
+    libqti-perfd-client \
+    libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
@@ -374,6 +375,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/google/pixel/power-libperfmgr \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
